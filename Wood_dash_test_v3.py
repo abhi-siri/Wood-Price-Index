095c5a152b284355282e1990ca7e7961c6ae7b64
Abhishek Verma <44469111+abhi-siri@users.noreply.github.com>
2025-05-06 13:15:44 +0530

    Update Wood_dash_test_v3.py

--- Wood_dash_test_v3.py
+++ Wood_dash_test_v3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 from xlsxwriter import Workbook
-
+from datetime import datetime
 # Initialize session state for the main data
 if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame(columns=[
@@ -10,34 +10,27 @@ if 'df' not in st.session_state:
         "Wood Collection Zone", "Supplied Mill", "SUPPLIER PO RATE", "SUB SUPPLIER WB RATE", 
         "Freight", "Balance", "Company Stock (in ASMT)","No_of_Trucks(Average)"
     ])
-
 # Title
 st.title("Weekly Wood Price Index - Paper Mills")
-
 # Tab layout for entry and categorized display
 tab1, tab2 = st.tabs(["Weekly Wood Price Index", "Transport rates"])
-
 with tab1:
     # Input fields and form layout
     col1, col2 = st.columns(2)
-
     with col1:
         # Date Selection
-        date = st.date_input("Select Date")
-        
+        date = st.date_input("Select Date")        
         # Wood Species Selection
         st.markdown("### Select Wood Species:")
         wood_species = st.selectbox(
             "Select Wood Species:",
             ["Select Wood Species"] + ["Acacia Wood Debarked","Bamboo","Casurina Wood","DeBark Subabul", "Gliricidia with Bark Wood","Melia Dubia Wood With Bark","With Bark Subabul", "With Bark Eucalyptus", "Debark Eucalyptus","Veneer Waste","Wood Rolls","Wood Waste","Wood Chips"]
-        )
-        
+        )        
         # Dropdown for Wood Price Source
         # wood_price_source = st.selectbox(
         #     "Select Wood Price Source:",
         #     ["Select Wood Price Source"] + ["CPM Unit", "SPM Unit", "JKPM Unit"]
-        # )
-        
+        # )        
         # State and District Selection
         zones = ["Zone 1", "Zone 2", "Zone 3"]
         state_district_map = {
@@ -81,10 +74,8 @@ with tab1:
                       "Muzaffarnagar", "Pilibhit", "Pratapgarh", "Prayagraj", "Raebareli", "Rampur", "Saharanpur", 
                       "Sambhal", "Sant Kabir Nagar", "Sant Ravidas Nagar", "Shahjahanpur", "Shamli", "Shrawasti", 
                       "Siddharthnagar", "Sitapur", "Sonbhadra", "Sultanpur", "Unnao", "Varanasi"]
-        }
-        
-        selected_state = st.selectbox("Select State:", ["Select State"] + list(state_district_map.keys()))
-        
+        }        
+        selected_state = st.selectbox("Select State:", ["Select State"] + list(state_district_map.keys()))        
         selected_districts = []
         selected_zone = None
         if selected_state != "Select State":
@@ -96,15 +87,13 @@ with tab1:
                 selected_zone = st.multiselect(
                     "Select Zone for the Selected Districts:", 
                     ["Select Zone"] + zones
-                )
-        
+                )    
         supplied_mill = st.multiselect(
             "Select Supplied Mill:",
             ["Select Supplied Mill"] + ["JK-CPM", "JK-SPM", "JKPM", "APL",
                                         "BILT","TNPL","Seshasayee","West Coast","ITC","Merino", "Orient",
                                         "HariHar","Green Panel","Century PLY","PLY/OTHERS"]
         )
-
     with col2:
         # Supplier Data Input
         st.markdown("### Enter Supplier Data")
@@ -114,11 +103,13 @@ with tab1:
         balance = supplier_rate - freight
         company_stock = st.number_input("Enter Company Stock (in ASMT):", value=0)
         no_of_trucks = st.number_input("Enter Average No. of Trucks:",value=0)
- 
-
     # Add Row Button
     if st.button("Add Row"):
-        if (
+        if supplier_rate<0 or sub_supplier_rate<0 or freight < 0 or no_of_trucks <0 or company_stock <0:
+            st.warning("Please ensure supplier data should be valid")
+        elif sub_supplier_rate>supplier_rate:
+            st.warning("Sub Supplier WB Rate Must be less than or Equal to  Supplier PO Rate")
+        elif (
             date and 
             wood_species != "Select Wood Species" and 
             # wood_price_source != "Select Wood Price Source" and 
@@ -128,8 +119,8 @@ with tab1:
             supplied_mill != "Select Supplied Mill" and 
             supplier_rate > 0 and 
             sub_supplier_rate > 0 and 
-            freight > 0
-        
+            freight > 0 and 
+            sub_supplier_rate < supplier_rate       
         ):
             new_row = {
                 "Date": date, 
@@ -150,46 +141,21 @@ with tab1:
                 st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame([new_row])], ignore_index=True)
             else:
                 # If the DataFrame is empty or doesn't exist, create it with the new row
-                st.session_state.df = pd.DataFrame([new_row])
-            
+                st.session_state.df = pd.DataFrame([new_row])            
             st.success("Row added successfully!")
-            # # Check if the DataFrame exists and is not empty
-            # if not st.session_state.df.empty:
-            #     # Check if all the values in the new row already exist in the DataFrame
-            #     if not st.session_state.df[
-            #         (st.session_state.df["Date"] == new_row["Date"]) &
-            #         (st.session_state.df["Wood Species"] == new_row["Wood Species"]) &
-            #         (st.session_state.df["Wood Collection Location"] == new_row["Wood Collection Location"]) &
-            #         # (st.session_state.df["Wood Collection Zone"] == new_row["Wood Collection Zone"]) &
-            #         (st.session_state.df["SUPPLIER PO RATE"] == new_row["SUPPLIER PO RATE"]) &
-            #         (st.session_state.df["SUB SUPPLIER WB RATE"] == new_row["SUB SUPPLIER WB RATE"]) &
-            #         (st.session_state.df["Freight"] == new_row["Freight"]) &
-            #         (st.session_state.df["Balance"] == new_row["Balance"]) &
-            #         (st.session_state.df["Company Stock (in ASMT)"] == new_row["Company Stock (in ASMT)"]) &
-            #         (st.session_state.df["No_of_Trucks(Average)"] == new_row["No_of_Trucks(Average)"])
-            #     ].empty:
-            #         st.warning("Duplicate row detected. This row already exists.")
-            #     else:
-            #         # Append the new row to the DataFrame if it is not a duplicate
-            #         st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame([new_row])], ignore_index=True)
-            #         st.success("Row added successfully!")
-            # else:
-            #         # If the DataFrame is empty, add the new row directly
-            #     st.session_state.df = pd.DataFrame([new_row])
-            #     st.success("Row added successfully!")
+            current_timestamp = datetime.now()
+            formatted_timestamp = current_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            st.success(f"Last Row Updated at:{formatted_timestamp}")
     else:
         st.warning("All fields are mandatory. Please fill in all required fields.")
-
     # Display DataFrame
     st.dataframe(st.session_state.df, use_container_width=True)
-
     # Function to convert dataframe to Excel
     def convert_df_to_excel(df):
         output = BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             df.to_excel(writer, index=False, sheet_name='Sheet1')
         return output.getvalue()
-
     # Download Button for Excel
     if not st.session_state.df.empty:
         st.download_button(
@@ -198,11 +164,9 @@ with tab1:
             file_name="wood_procurement_data.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
-
 with tab2:
     # Categorized Data Display
     st.markdown("### Transport Rates")
-
     if not st.session_state.df.empty:
         # Filter data for Subabul and Eucalyptus
         subabul_data = st.session_state.df[
@@ -250,9 +214,7 @@ with tab2:
         wood_rolls_data["Wood Collection Location"] = wood_rolls_data["Wood Collection Location"].astype(str) + " - " + wood_rolls_data["Wood Collection Zone"].astype(str)
         wood_waste_data["Wood Collection Location"] = wood_waste_data["Wood Collection Location"].astype(str) + " - " + wood_waste_data["Wood Collection Zone"].astype(str)
         wood_chips_data["Wood Collection Location"] = wood_chips_data["Wood Collection Location"].astype(str) + " - " + wood_chips_data["Wood Collection Zone"].astype(str)
-
-        # Select relevant columns to display
-        # Creating display columns for all datasets
+    
         subabul_data_display = subabul_data[["Date", "Wood Collection Location", "Freight"]]
         eucalyptus_data_display = eucalyptus_data[["Date", "Wood Collection Location", "Freight"]]
         acacia_data_display = acacia_data[["Date", "Wood Collection Location", "Freight"]]
@@ -264,42 +226,29 @@ with tab2:
         wood_rolls_data_display = wood_rolls_data[["Date", "Wood Collection Location", "Freight"]]
         wood_waste_data_display = wood_waste_data[["Date", "Wood Collection Location", "Freight"]]
         wood_chips_data_display = wood_chips_data[["Date", "Wood Collection Location", "Freight"]]
-
-
         # Display Wood Data
         # Display the dataframes in the Streamlit app
         st.markdown("#### Subabul")
         st.dataframe(subabul_data_display, use_container_width=True)
-
         st.markdown("#### Eucalyptus")
         st.dataframe(eucalyptus_data_display, use_container_width=True)
-
         st.markdown("#### Acacia")
         st.dataframe(acacia_data_display, use_container_width=True)
-
         st.markdown("#### Casurina")
         st.dataframe(casurina_data_display, use_container_width=True)
-
         st.markdown("#### Gliricidia")
         st.dataframe(gliricidia_data_display, use_container_width=True)
-
         st.markdown("#### Melia")
         st.dataframe(melia_data_display, use_container_width=True)
-
         st.markdown("#### Bamboo")
         st.dataframe(bamboo_data_display, use_container_width=True)
-
         st.markdown("#### Veneer Waste")
         st.dataframe(veneer_waste_data_display, use_container_width=True)
-
         st.markdown("#### Wood Rolls")
         st.dataframe(wood_rolls_data_display, use_container_width=True)
-
         st.markdown("#### Wood Waste")
         st.dataframe(wood_waste_data_display, use_container_width=True)
-
         st.markdown("#### Wood Chips")
         st.dataframe(wood_chips_data_display, use_container_width=True)
-
     else:
         st.warning("No data available. Please add rows in the Data Entry tab.")
